a898803f20892db132d0d8f1a4a749c402b88199
EliasYoc <eliyoc_97@hotmail.com>
2021-07-06 11:18:55 -0500

    mejoras de diseño y conexion lenta

--- src/components/MessageError.js
+++ src/components/MessageError.js
@@ -1,21 +1,37 @@
+import { useEffect, useState } from "react";
 import rickLaughing from "../assets/rick-morty-gifs/rickLaughing.gif";
 const MessageError = ({ text }) => {
+  const [hasBadConnection, setHasBadConnection] = useState(false);
   let containerError = {
-    padding: "0 10px",
+    padding: `${hasBadConnection ? "20px" : "0px"} 10px`,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     color: "var(--white-to-black)",
     backgroundColor: "#bf2440",
   };
+  useEffect(() => {
+    // console.log(hasBadConnection);
+    // console.log(navigator.connection);
+    const connection = navigator.connection;
+    if (connection.effectiveType === "2g" && connection.downlink < 1) {
+      setHasBadConnection(true);
+      // console.log("hay mala conexion");
+    } else {
+      setHasBadConnection(false);
+      // console.log("buena conexion");
+    }
+  }, [hasBadConnection]);
   return (
     <div style={containerError} className="container-error">
       <p>{text}, ¿en serio creíste que lo hallarías?</p>
-      <img
-        style={{ width: "150px", height: "150px" }}
-        src={rickLaughing}
-        alt=""
-      />
+      {!hasBadConnection && (
+        <img
+          style={{ width: "150px", height: "150px" }}
+          src={rickLaughing}
+          alt=""
+        />
+      )}
     </div>
   );
 };
--- src/components/ToggleDarkMode.js
+++ src/components/ToggleDarkMode.js
@@ -1,17 +1,10 @@
-import { useRef } from "react";
 import "./ToggleDarkMode.css";
 const ToggleDarkMode = () => {
-  const refToggle = useRef();
-
-  const toggle = refToggle.current;
-
   const handleToggle = () => {
-    console.log(toggle);
-    // toggle.classList.toggle("active");
     document.body.classList.toggle("dark");
   };
   return (
-    <div onClick={handleToggle} ref={refToggle} className="toggle">
+    <div onClick={handleToggle} className="toggle">
       <span className="toggle__switch"></span>
 
       <div className="toggle__container">
